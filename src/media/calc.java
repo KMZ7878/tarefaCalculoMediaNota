f1602a08340fdbd781fe528f6dc215e3fb0f7b26
KMZ7878 <marqueskaue771@gmail.com>
2024-06-06 15:21:03 -0300

    Update calc.java

--- src/media/calc.java
+++ src/media/calc.java
@@ -4,21 +4,19 @@ public class calc {
 
 	public static void main(String[] args) {
 		int nota0 = 5;
-		
+
 		int nota1 = 5;
-		
+
 		int nota2 = 6;
-		
+
 		int nota3 = 8;
-		
+
 		int soma = (nota0 + nota1 + nota2 + nota3) / 4;
 		System.out.println(soma);
 		if (soma >= 6) {
 			System.out.println("passou!");
-		}else
-		System.out.println("não passou");
-		
-				
+		} else
+			System.out.println("não passou");
 
 	}
 
